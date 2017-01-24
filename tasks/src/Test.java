23ae4c7e8b15818a87f1bfb625967ca427edc639
after <Dtcyf2016>
2017-01-24 21:26:40 +0200

    add document.xml generator

--- tasks/src/Test.java
+++ tasks/src/Test.java
@@ -5,24 +5,38 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) throws IOException, XmlException {
-        Scanner scanner = new Scanner(new File("/home/after/ProjectNTU/tasksad/tasks/src/task_ad/word/document.xml"));
+        Scanner scanner = new Scanner(new File("/home/after/ProjectNTU/tasksad/tasks/src/Patterns/front.xml"));
         String sFront = "";
         String body = "";
         while(scanner.hasNext()){
             sFront = sFront + scanner.nextLine()+"\n";
         }
-        body = sFront.substring(sFront.indexOf("<w:body>")+8,sFront.indexOf("</w:body>"));
+        sFront = sFront.substring(sFront.indexOf("<w:body>")+8,sFront.indexOf("</w:body>"));
+        System.out.println(sFront);
 
-        scanner = new Scanner(new File("/home/after/ProjectNTU/tasksad/tasks/src/task_ad_back/word/document.xml"));
+        scanner = new Scanner(new File("/home/after/ProjectNTU/tasksad/tasks/src/Patterns/back.xml"));
         String sBack = "";
-        String bodyBack = "";
         while(scanner.hasNext()){
             sBack = sBack + scanner.nextLine()+"\n";
         }
-        bodyBack = sBack.substring(sBack.indexOf("<w:body>")+8,sBack.indexOf("</w:body>"));
+        sBack = sBack.substring(sBack.indexOf("<w:body>")+8,sBack.indexOf("</w:body>"));
+        System.out.println(sBack);
 
-        sFront = sFront.replaceAll(sFront.substring(sFront.indexOf("<w:body>")+8,sFront.indexOf("</w:body>")), body);
-        System.out.println(body);
+        String form = "";
+        scanner = new Scanner(new File("/home/after/ProjectNTU/tasksad/tasks/src/Patterns/form/word/document.xml"));
+        while(scanner.hasNext()){
+            form = form + scanner.nextLine()+"\n";
+        }
+        System.out.println(form);
+
+        body = sFront+sBack;
+        form = form.replaceAll("BODY", body);
+
+        System.out.println(form);
+
+        FileWriter fw = new FileWriter("/home/after/ProjectNTU/tasksad/tasks/src/document.xml");
+        fw.write(form);
+        fw.flush();
     }
 }
 
